0258cd0bd5df60b9bb5810523ddb5a3427315200
Stefan van Groningen <groninge@hotmail.com>
2018-09-28 20:19:12 +0200

    Modified routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
